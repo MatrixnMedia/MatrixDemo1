12c5ec284e8afa08105ccbc50d6c7fc9021866f8
parna <parna@matrixnmedia.com>
2016-01-20 13:20:45 +0530

    commit 3 parna

--- app/src/main/java/app/matrixdemo1/SecondActivity.java
+++ app/src/main/java/app/matrixdemo1/SecondActivity.java
@@ -11,5 +11,6 @@ public class SecondActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
     }
 }
