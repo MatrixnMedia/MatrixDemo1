219c088447228d7116aad05b3ee6cfb2d673b520
parna <parna@matrixnmedia.com>
2016-01-20 13:12:51 +0530

    commit 2

--- app/src/main/java/app/matrixdemo1/MainActivity.java
+++ app/src/main/java/app/matrixdemo1/MainActivity.java
@@ -1,5 +1,7 @@
 package app.matrixdemo1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +10,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +60,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn:
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/app/matrixdemo1/SecondActivity.java
@@ -0,0 +1,15 @@
+package app.matrixdemo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by parna on 20/1/16.
+ */
+public class SecondActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
