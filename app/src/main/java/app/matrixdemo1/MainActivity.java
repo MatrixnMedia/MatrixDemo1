35a90d338d1c93488ee56932280ccd819d8d2018
Sutapa <smartdevindiana@gmail.com>
2016-01-20 15:37:17 +0530

    Log added

--- app/src/main/java/app/matrixdemo1/MainActivity.java
+++ app/src/main/java/app/matrixdemo1/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,8 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Log.e("hello","sutapa");
             }
         });
     }
